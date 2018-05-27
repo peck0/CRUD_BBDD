1c38d3bd85430fdc7854b7f59a831faa3cf8da8c
infante <infpedro@gmail.com>
2018-05-27 15:23:50 -0400

    agregar dos botones

--- app/src/main/java/com/example/infante/crud_bbdd/NuevaSangria.java
+++ app/src/main/java/com/example/infante/crud_bbdd/NuevaSangria.java
@@ -9,5 +9,7 @@ public class NuevaSangria extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.nueva_sangria);
+
+        int billetes_1000, billetes_500,billetes_200, billetes_100, billetes_50;
     }
 }
